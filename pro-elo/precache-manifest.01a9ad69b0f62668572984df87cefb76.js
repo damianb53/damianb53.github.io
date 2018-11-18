babfe7c3a7dc03ba5ea09d776cf6aefd89b39743
damianb53 <damianb53@outlook.com>
2018-11-18 22:51:58 +0100

    added pro-elo version

---
+++ pro-elo/precache-manifest.01a9ad69b0f62668572984df87cefb76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f3a7776c6ec21aec9ba",
+    "url": "/static/js/main.8f3a7776.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "8f3a7776c6ec21aec9ba",
+    "url": "/static/css/main.c8ee9c21.chunk.css"
+  },
+  {
+    "revision": "d847f6df0be817e041b2468a077669ba",
+    "url": "/index.html"
+  }
+];
